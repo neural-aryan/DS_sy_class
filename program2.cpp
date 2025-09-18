a47a901fb6ed5271f9d35eb529f9744fa6c9454b
neural-aryan <aryanbajpai296@gmail.com>
2025-09-18 23:32:51 +0530

    Add files via upload

---
+++ program2.cpp
@@ -0,0 +1,14 @@
+//write a program to calculate sum of all elements in an array
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    int arr[5]={1,2,3,4,5};
+    int sum=0;
+    for(int i=0; i<5; i++){
+        sum+=arr[i];
+    }
+    cout<<"sum of elements are:"<<sum<<endl;
+    return 0; 
+}

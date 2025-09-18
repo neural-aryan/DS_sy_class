813530bce5be07c8e8e9c5386b5a63f075a52b07
neural-aryan <aryanbajpai296@gmail.com>
2025-09-18 23:34:31 +0530

    Add files via upload

---
+++ program3.cpp
@@ -0,0 +1,24 @@
+//write a program to represent 3x3 matrix in the form of 2d array
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[3][3];
+
+    cout << "Enter elements of 3x3 matrix:"<<endl;
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            cin >> arr[i][j];
+        }
+    }
+
+    cout << "Display:"<<endl;
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            cout << arr[i][j] << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
